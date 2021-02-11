13ae2b5ceed1b1b2c6f5979a8d9f75fb5aac7ba6
DanielaZabaleta <zabaleta.daniela@uces.edu.co>
2021-02-11 07:39:03 -0500

    Clase 2

---
+++ Clases/Operaciones.py
@@ -0,0 +1,13 @@
+NumeroA = 87
+NumeroB = 83
+sumar = NumeroA + NumeroB 
+print ("el resultado de la suma es", sumar)
+print (f"la suma dio {sumar} exitoasamente")
+resta = NumeroA - NumeroB
+print ("el resultado de la resta es", resta)
+multiplicar = NumeroA * NumeroB
+print ("el resultado de la multiplicacion es", multiplicar)
+dividir = NumeroA / NumeroB
+print ("el resultado de la divison es", dividir)
+exponente = NumeroA**NumeroB
+print ("el resultado del exponente es", exponente)
