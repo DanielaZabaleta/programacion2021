150a54f28db7020288d58ce3005a0bf77174e96c
DanielaZabaleta <zabaleta.daniela@uces.edu.co>
2021-02-16 19:05:54 -0500

    Realizando el taller 2

--- Talleres/Taller 2.py
+++ Talleres/Taller 2.py
@@ -1,2 +1,15 @@
-PREGUNTA_NUMEROA = "Ingrese un Numero A"
-PREGUNTA_NUMEROB = "Ingrese un Numero B"
+#-----Constantes-----#
+PREGUNTA_NOMBRE = "Como te llamas? :"
+MENSAJE_SALUDO = "Un gusto en conocerte"
+ENUNCIADO = "Ayudanos llenando el siguiente formato"
+PREGUNTA_NUMEROA = "Ingrese un Numero A :"
+PREGUNTA_NUMEROB = "Ingrese un Numero B :"
+
+#-----Entrada al codigo-----#
+Nombre = input (PREGUNTA_NOMBRE)
+print (MENSAJE_SALUDO, Nombre)
+print (ENUNCIADO)
+NumeroA = int (input (PREGUNTA_NUMEROA))
+NumeroB = int (input (PREGUNTA_NUMEROB))
+isNumeroMayor = PREGUNTA_NUMEROA > PREGUNTA_NUMEROB
+print (isNumeroMayor)
