e357c52f7ef408e4821c5befd27ef849fc689fa0
DanielaZabaleta <zabaleta.daniela@uces.edu.co>
2021-02-16 22:17:36 -0500

    Realizando operaciones del Taller 2

--- Talleres/Taller 2.py
+++ Talleres/Taller 2.py
@@ -25,3 +25,13 @@ isNumerosDiferentes = NumeroA != NumeroB
 print (isNumerosDiferentes)
 
 #-----Operaciones-----#
+Sumar = NumeroA + NumeroB
+print ("el resultado de la suma es", Sumar)
+Restar = NumeroA - NumeroB 
+print ("el resultado de la resta es", Restar)
+Multiplicar = NumeroA * NumeroB 
+print ("el resultado de la multiplicacion es", Multiplicar )
+Dividir = NumeroA / NumeroB 
+print ("el resultado de la division es", Dividir)
+Exponente = NumeroA**NumeroB 
+print ("el resultado del exponente es", Exponente)
