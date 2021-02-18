2647b3ebfdb61d2e78ea0206f970f2f8980c8480
DanielaZabaleta <zabaleta.daniela@uces.edu.co>
2021-02-18 07:33:19 -0500

    Practicando nuevos codigos

---
+++ Clases/edad.py
@@ -0,0 +1,9 @@
+#----Constantes----#
+MENSAJE_BIENVENIDA = "Bienvenido al codigo"
+MENSAJE_MAYOR_EDAD = "Eres mayor de edad puedes seguir"
+MENSAJE_MENOR_EDAD = "Eres menor de edad no puedes seguir"
+PREGUNTA_EDAD = "Cuantos años tienes? :"
+
+#-----Entrada al codigo-----#
+print (MENSAJE_BIENVENIDA)
+edad = int (inout(PREGUNTA_EDAD))
