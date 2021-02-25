0e259294be0c52bfb3d194a57fedbb40d437e0ed
DanielaZabaleta <zabaleta.daniela@uces.edu.co>
2021-02-25 07:51:49 -0500

    Aprendiendo Ciclo While

--- Clases/Juego1.py
+++ Clases/Juego1.py
@@ -15,7 +15,7 @@ while (NumeroOculto != NumeroIngresado and Vidas > 0):
     NumeroIngresado = int (input(PREGUNTA_FALLIDA))
     Vidas -=1
 
-if (Vidas > 0):
+if (Vidas >= 0):
     print (MENSAJE_GANASTE)
     print (Vidas)
 else:
