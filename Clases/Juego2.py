bb8237fb7a7a64e62f719416d01a161b4609a57a
DanielaZabaleta <zabaleta.daniela@uces.edu.co>
2021-03-04 07:54:56 -0500

    Terminando el juego While

--- Clases/Juego2.py
+++ Clases/Juego2.py
@@ -2,6 +2,9 @@
 import random
 #----Entradas----#
 MENSAJE_SALUDAR = '''Bienvenido a este programa, juguemos!!'''
+MENSAJE_SEGUNDO_NIVEL  =  'Felicidades pasaste el primer nivel ahora ve por el último !!'
+MENSAJE_CALIENTE  =  'Estas caliente'
+MENSAJE_FRIO  =  'Estas Frio'
 PREGUNTAR_NUMERO = 'En este juego debes acertar un numero entero que va desde el 1-10, la idea es que antes de que se te acaben las vidas, no existe una vida 0... Muchos exitos, ingresa tu numero : '
 PREGUNTA_DIFICULTAD = '''
     1- Facil
@@ -14,7 +17,9 @@ MENSAJE_GANASTE = 'Felicidades ganaste!!'
 MENSAJE_PERDISTE = 'Perdiste, vuelve a intentarlo!!'
 #----Entrada al codigo----#
 NumeroOculto = random.randint(1,10)
+NumeroOcultoDos = random.randint (1,10)
 Vidas = None
+
 Dificultad = int (input (PREGUNTA_DIFICULTAD))
 while (Dificultad !=1 and Dificultad !=2 and Dificultad !=3):
     print ('Ingrese una opcion valida')
@@ -22,21 +27,38 @@ while (Dificultad !=1 and Dificultad !=2 and Dificultad !=3):
 
 
 if (Dificultad == 1):
-    Vidas = 5
+    Vidas = 10
     print ('Modo facil activado')
 elif (Dificultad == 2):
-    Vidas = 3
+    Vidas = 5
     print ('Modo moderado activado')
 else:
-    Vidas = 1
+    Vidas = 2
     print ('Modo dificil actvidado. significa peligro')
+
 NumeroIngresado = int(input(PREGUNTAR_NUMERO))
 while (NumeroIngresado != NumeroOculto and Vidas > 1):
+    if (NumeroIngresado > NumeroOculto):
+        print (MENSAJE_CALIENTE)
+    else:
+        print (MENSAJE_FRIO)
     Vidas -= 1
     print (f'Te quedan {Vidas} vidas')
     NumeroIngresado = int (input (PREGUNTA_FALLIDA))
+if (Vidas >= 0 and NumeroIngresado == NumeroOculto):
+    print (MENSAJE_SEGUNDO_NIVEL)
+    NumeroIngresado = int (input (PREGUNTAR_NUMERO))
+    while (NumeroIngresado != NumeroOcultoDos  and Vidas > 1 ):
+        if (NumeroIngresado > NumeroOcultoDos ):
+            print (MENSAJE_CALIENTE)
+        else:
+            print (MENSAJE_FRIO)
+        Vidas -= 1
+        print (f'Te quedan {Vidas} vidas')
+        NumeroIngresado = int (input (PREGUNTA_FALLIDA))
 
-if (Vidas >= 0  and NumeroIngresado == NumeroOculto):
+if (Vidas >= 0  and NumeroIngresado == NumeroOcultoDos):
     print (MENSAJE_GANASTE)
 else: 
-    print (MENSAJE_PERDISTE, 'El numero era el', NumeroOculto)
+    print (MENSAJE_PERDISTE, 'El numero era el', NumeroOculto,
+    'El numero oculto dos era el', NumeroOcultoDos)
