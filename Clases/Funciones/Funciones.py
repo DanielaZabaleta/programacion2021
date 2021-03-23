4127236a32f520d2a4700b038f9b9415db2803d2
DanielaZabaleta <zabaleta.daniela@uces.edu.co>
2021-03-23 09:11:53 -0500

    Clase de funciones

---
+++ Clases/Funciones/Funciones.py
@@ -0,0 +1,23 @@
+#----Sumar 2 numeros----#
+def sumar (a =0,b = 0):
+    suma = a + b
+    return suma 
+#----Restar 2 numeros----#
+def restar (a =0,b = 0):
+    resta = a - b
+    return resta
+#----Multiplicar 2 numeros----#
+def multiplicar (a =0,b = 0):
+    multiplica = a * b
+    return multiplica 
+#----Dividir 2 numeros----#
+def dividir (a = 0,b = 1):
+    dividi = a / b
+    return dividi
+#----Potenciar 2 numeros----#
+def potenciar (base = 0,exponente = 1):
+    potencia = base ** exponente
+    return potencia
+#----Funciones dependientes de otras----#
+def calcular (operacion,NumeroA,NumeroB):
+    print (operacion(NumeroA,NumeroB))
